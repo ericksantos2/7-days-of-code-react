635c3bf4e0ebb43b19dcd345a8789967478483e2
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-03-17 12:30:58 -0300

    removi o conteudo e adicionei as linhas de fundo que tem no figma

--- src/pages/styled.jsx
+++ src/pages/styled.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { ReactComponent as VectorSvg } from '@/assets/vector.svg';
-import * as variaveis from '@/styles/variaveis.jsx';
+import { margem } from '@/styles/variaveis.jsx';
 
 const Imagem = styled.img`
   position: absolute;
@@ -13,33 +13,38 @@ const Vector = styled(VectorSvg)`
   right: 0;
 `;
 
+const Linhas = styled.span`
+  ::before {
+    height: ${document.body.scrollHeight}px;
+    min-width: 100%;
+    background-image: url(/assets/linhas.svg);
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
 const ImagemDiv = styled.div`
   position: relative;
-  margin: 0 ${variaveis.margem};
+  margin: 0 ${margem};
 `;
 
 const BackgroundDiv = styled.div`
   position: absolute;
   top: 0;
-  right: 0;
-`
-
-export const Conteudo = styled.div`
   width: 100%;
-  // position: absolute;
-  top: 0;
-  left: 0;
+  right: 0;
 `;
 
 function ImagemItem() {
-  return (
-    <Imagem src='/imagens/imagem-hero1.png' alt='Imagem de uma árvore' />
-  );
+  return <Imagem src='/imagens/imagem-hero1.png' alt='Imagem de uma árvore' />;
 }
 
 export function Background() {
   return (
     <BackgroundDiv>
+      <Linhas />
       <Vector />
       <ImagemDiv>
         <ImagemItem />
