30b6696f1df7adcd3a575e71a25b64f267d41ab7
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-03-18 20:28:00 -0300

    arrumei o erro de top level await

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), topLevelAwait()],
   resolve: {
     alias: [
       {
